65c8b2d9c41555dc9c0586398290aa944efcecbd
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:36:40 +0500

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,14 @@
+// Q10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write 
+// because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+//  describing what the program does.
+
+let x: number = 5
+let y: number = 2
+// ADDITION
+console.log(`${x}+${y}\=${x+y}`);
+// SUBTRACTION
+console.log(`${x}-${y}\=${x-y}`);
+// MULTILICATION
+console.log(`${x}*${y}\=${x*y}`);
+// DIVISION
+console.log(`${x}/${y}\=${x/y}`);
